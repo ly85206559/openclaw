284aa160143908f2b7a472157ac91b26f638fb3f
ben.li <li.yang6@xydigit.com>
2026-09-08 10:24:44 +0800

    ci: capture PR 141163 Gateway survival proof

---
+++ .github/proof/pr-141163-inject.mjs
@@ -0,0 +1,9 @@
+const delayMs = Number(process.env.OPENCLAW_PROOF_INJECT_DELAY_MS ?? "30000");
+
+setTimeout(() => {
+  const error = Object.assign(new Error("PR 141163 transient DNS proof"), {
+    code: "EAI_AGAIN",
+  });
+  process.stderr.write("[proof] PROOF_TRANSIENT_INJECTED code=EAI_AGAIN\n");
+  void Promise.reject(error);
+}, delayMs);
