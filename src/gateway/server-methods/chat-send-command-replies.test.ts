ayan1666668-ops/open

fix(gateway): keep conflicting reply owners separate

src/gateway/server-methods/chat-send-command-replies.test.ts
@@ -3,6 +3,7 @@ import {
   getReplyPayloadMetadata,
   isReplyPayloadSessionWriterDeliveryAuthorized,
   setReplyPayloadMetadata,
+  type ReplyPayloadMetadata,
 } from "../../auto-reply/reply-payload.js";
 import { selectChatSendFinalReplyPayloads } from "./chat-send-command-replies.js";
 
@@ -10,6 +11,23 @@ const staleWriterAuthority = {
   expectedSessionId: "session-before-replacement",
   expectedWriterRunId: "run-before-replacement",
   sessionKey: "agent:main:webchat",
+} as const;
+
+const currentWriterAuthority = {
+  expectedSessionId: "replacement-session",
+  expectedWriterRunId: "replacement-run",
+  sessionKey: "agent:main:webchat",
+} as const;
+
+const blockedTranscriptMirror = {
+  expectedSessionId: "session-before-replacement",
+  sessionKey: "agent:main:source",
+  transcriptWriteBlocked: true,
+} as const;
+
+const currentTranscriptMirror = {
+  expectedSessionId: "replacement-session",
+  sessionKey: "agent:main:replacement",
 } as const;
 
 function expectStaleWriterRejected(payload: object) {
@@ -22,6 +40,32 @@ function expectStaleWriterRejected(payload: object) {
       sessionId: "replacement-session",
     }),
   ).toBe(false);
+}
+
+function selectDuplicateOwnerPayloads(
+  blockMetadata: ReplyPayloadMetadata,
+  finalMetadata: ReplyPayloadMetadata,
+) {
+  return selectChatSendFinalReplyPayloads({
+    deliveredReplies: [
+      {
+        kind: "block",
+        payload: setReplyPayloadMetadata(
+          { text: "done", mediaUrl: "file:///tmp/result.png" },
+          blockMetadata,
+        ),
+      },
+      {
+        kind: "final",
+        payload: setReplyPayloadMetadata(
+          { text: "done", mediaUrls: ["/tmp/result.png"] },
+          finalMetadata,
+        ),
+      },
+    ],
+    foldCommandBlocks: true,
+    suppressReplies: false,
+  });
 }
 
 describe("selectChatSendFinalReplyPayloads", () => {
@@ -134,5 +178,63 @@ describe("selectChatSendFinalReplyPayloads", () => {
       },
     ]);
     expectStaleWriterRejected(result[1]!);
+  });
+
+  it.each([
+    ["stale block", staleWriterAuthority, currentWriterAuthority],
+    ["stale final", currentWriterAuthority, staleWriterAuthority],
+  ])("keeps conflicting delivery owners separate with a %s", (_label, block, final) => {
+    const result = selectDuplicateOwnerPayloads(
+      { sessionWriterDeliveryAuthority: block },
+      { sessionWriterDeliveryAuthority: final },
+    );
+    expect(result).toHaveLength(2);
+    expect(getReplyPayloadMetadata(result[0]!)).toMatchObject({
+      sessionWriterDeliveryAuthority: block,
+    });
+    expect(getReplyPayloadMetadata(result[1]!)).toMatchObject({
+      sessionWriterDeliveryAuthority: final,
+    });
+    expect(
+      result.every((payload) =>
+        isReplyPayloadSessionWriterDeliveryAuthorized(payload, {
+          activeWriterRunId: "replacement-run",
+          sessionId: "replacement-session",
+        }),
+      ),
+    ).toBe(false);
+  });
+
+  it.each([
+    ["blocked block", blockedTranscriptMirror, currentTranscriptMirror],
+    ["blocked final", currentTranscriptMirror, blockedTranscriptMirror],
+  ])("keeps conflicting transcript owners separate with a %s", (_label, block, final) => {
+    const result = selectDuplicateOwnerPayloads(
+      { sourceReplyTranscriptMirror: block },
+      { sourceReplyTranscriptMirror: final },
+    );
+
+    expect(result).toHaveLength(2);
+    expect(getReplyPayloadMetadata(result[0]!)?.sourceReplyTranscriptMirror).toEqual(block);
+    expect(getReplyPayloadMetadata(result[1]!)?.sourceReplyTranscriptMirror).toEqual(final);
+  });
+
+  it("folds replies when both metadata carriers have the same owners", () => {
+    const result = selectDuplicateOwnerPayloads(
+      {
+        sessionWriterDeliveryAuthority: currentWriterAuthority,
+        sourceReplyTranscriptMirror: currentTranscriptMirror,
+      },
+      {
+        sessionWriterDeliveryAuthority: { ...currentWriterAuthority },
+        sourceReplyTranscriptMirror: { ...currentTranscriptMirror },
+      },
+    );
+
+    expect(result).toHaveLength(1);
+    expect(getReplyPayloadMetadata(result[0]!)).toMatchObject({
+      sessionWriterDeliveryAuthority: currentWriterAuthority,
+      sourceReplyTranscriptMirror: currentTranscriptMirror,
+    });
   });
 });

ly85206559/openclaw

(cherry picked from commit 7bbff2ff3272d23ae5fccb49491425fd945aa051)

src/gateway/server-methods/chat-send-command-replies.test.ts
@@ -7,6 +7,7 @@ import {
   isReplyPayloadSessionWriterDeliveryAuthorized,
   setReplyPayloadMetadata,
   type ReplyPayload,
+  type ReplyPayloadMetadata,
 } from "../../auto-reply/reply-payload.js";
 import type { ReplyDispatchOperation } from "../../auto-reply/reply/reply-dispatcher.types.js";
 import { createReplyToModeFilterForChannel } from "../../auto-reply/reply/reply-threading.js";
@@ -40,6 +41,33 @@ const staleWriterAuthority = {
   sessionKey: "agent:main:webchat",
 } as const;
 
+const currentWriterAuthority = {
+  expectedSessionId: "replacement-session",
+  expectedWriterRunId: "replacement-run",
+  harnessCompletion: {
+    lifecycleRevision: "revision-1",
+    requesterAgentId: "main",
+    requesterSessionKey: "agent:main:webchat",
+    sessionId: "replacement-session",
+    sourceRunId: "source-run",
+    taskId: "task-1",
+    taskRunId: "task-run-1",
+    taskStatus: "succeeded",
+  },
+  sessionKey: "agent:main:webchat",
+} as const;
+
+const blockedTranscriptMirror = {
+  expectedSessionId: "session-before-replacement",
+  sessionKey: "agent:main:source",
+  transcriptWriteBlocked: true,
+} as const;
+
+const currentTranscriptMirror = {
+  expectedSessionId: "replacement-session",
+  sessionKey: "agent:main:replacement",
+} as const;
+
 function expectStaleWriterRejected(payload: object) {
   expect(getReplyPayloadMetadata(payload)).toMatchObject({
     sessionWriterDeliveryAuthority: staleWriterAuthority,
@@ -50,6 +78,32 @@ function expectStaleWriterRejected(payload: object) {
       sessionId: "replacement-session",
     }),
   ).toBe(false);
+}
+
+function selectDuplicateOwnerPayloads(
+  blockMetadata: ReplyPayloadMetadata,
+  finalMetadata: ReplyPayloadMetadata,
+) {
+  return selectRawReplies({
+    deliveredReplies: [
+      {
+        kind: "block",
+        payload: setReplyPayloadMetadata(
+          { text: "done", mediaUrl: "file:///tmp/result.png" },
+          blockMetadata,
+        ),
+      },
+      {
+        kind: "final",
+        payload: setReplyPayloadMetadata(
+          { text: "done", mediaUrls: ["/tmp/result.png"] },
+          finalMetadata,
+        ),
+      },
+    ],
+    foldCommandBlocks: true,
+    suppressReplies: false,
+  });
 }
 
 describe("selectChatSendFinalReplyInputs", () => {
@@ -263,6 +317,87 @@ describe("selectChatSendFinalReplyInputs", () => {
       },
     ]);
     expectStaleWriterRejected(replies[1]!);
+  });
+
+  it.each([
+    ["stale block", staleWriterAuthority, currentWriterAuthority],
+    ["stale final", currentWriterAuthority, staleWriterAuthority],
+  ])("keeps conflicting raw delivery owners separate with a %s", (_label, block, final) => {
+    const replies = selectDuplicateOwnerPayloads(
+      { sessionWriterDeliveryAuthority: block },
+      { sessionWriterDeliveryAuthority: final },
+    );
+    expect(replies).toHaveLength(2);
+    expect(getReplyPayloadMetadata(replies[0]!)).toMatchObject({
+      sessionWriterDeliveryAuthority: block,
+    });
+    expect(getReplyPayloadMetadata(replies[1]!)).toMatchObject({
+      sessionWriterDeliveryAuthority: final,
+    });
+    expect(
+      replies.every((payload) =>
+        isReplyPayloadSessionWriterDeliveryAuthorized(payload, {
+          activeWriterRunId: "replacement-run",
+          sessionId: "replacement-session",
+        }),
+      ),
+    ).toBe(false);
+  });
+
+  it("keeps distinct harness completion claims separate", () => {
+    const replies = selectDuplicateOwnerPayloads(
+      { sessionWriterDeliveryAuthority: currentWriterAuthority },
+      {
+        sessionWriterDeliveryAuthority: {
+          ...currentWriterAuthority,
+          harnessCompletion: {
+            ...currentWriterAuthority.harnessCompletion,
+            taskRunId: "task-run-2",
+          },
+        },
+      },
+    );
+
+    expect(replies).toHaveLength(2);
+    expect(
+      getReplyPayloadMetadata(replies[0]!)?.sessionWriterDeliveryAuthority?.harnessCompletion
+        ?.taskRunId,
+    ).toBe("task-run-1");
+    expect(
+      getReplyPayloadMetadata(replies[1]!)?.sessionWriterDeliveryAuthority?.harnessCompletion
+        ?.taskRunId,
+    ).toBe("task-run-2");
+  });
+
+  it.each([
+    ["blocked block", blockedTranscriptMirror, currentTranscriptMirror],
+    ["blocked final", currentTranscriptMirror, blockedTranscriptMirror],
+  ])("keeps conflicting raw transcript owners separate with a %s", (_label, block, final) => {
+    const replies = selectDuplicateOwnerPayloads(
+      { sourceReplyTranscriptMirror: block },
+      { sourceReplyTranscriptMirror: final },
+    );
+    expect(replies).toHaveLength(2);
+    expect(getReplyPayloadMetadata(replies[0]!)?.sourceReplyTranscriptMirror).toEqual(block);
+    expect(getReplyPayloadMetadata(replies[1]!)?.sourceReplyTranscriptMirror).toEqual(final);
+  });
+
+  it("folds raw replies when both metadata carriers have the same owners", () => {
+    const replies = selectDuplicateOwnerPayloads(
+      {
+        sessionWriterDeliveryAuthority: currentWriterAuthority,
+        sourceReplyTranscriptMirror: currentTranscriptMirror,
+      },
+      {
+        sessionWriterDeliveryAuthority: { ...currentWriterAuthority },
+        sourceReplyTranscriptMirror: { ...currentTranscriptMirror },
+      },
+    );
+    expect(replies).toHaveLength(1);
+    expect(getReplyPayloadMetadata(replies[0]!)).toMatchObject({
+      sessionWriterDeliveryAuthority: currentWriterAuthority,
+      sourceReplyTranscriptMirror: currentTranscriptMirror,
+    });
   });
 
   it.each([